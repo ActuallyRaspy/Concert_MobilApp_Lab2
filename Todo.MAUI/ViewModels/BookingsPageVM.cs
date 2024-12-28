72aaeb37188299a776d247f6475ece86bdc19761
Arvid <159460210+Arv11d@users.noreply.github.com>
2024-12-28 16:25:39 +0100

    search option for bookings
    searches by mail

--- Todo.MAUI/ViewModels/BookingsPageVM.cs
+++ Todo.MAUI/ViewModels/BookingsPageVM.cs
@@ -14,19 +14,39 @@ namespace Todo.MAUI.ViewModels
     public partial class BookingsPageVM : ObservableObject
     {
         private readonly IBookingService _bookingService;
+        private readonly IPerformanceService _performanceService;
+        private readonly IConcertService _concertService;
+
+
 
         [ObservableProperty]
-        public string text;
+        public string text="";
         [ObservableProperty]
         public ICollection<Booking> bookings;
+
         [RelayCommand]
         public async Task Test()
         {
-            Bookings = await _bookingService.GetBookingsAsync();
+            var bookings = await _bookingService.GetBookingsAsync();
+            var performances = await _performanceService.GetPerformancesAsync();
+            var concerts = await _concertService.GetConcertsAsync();
+            if (Text.Length>0)
+            bookings = bookings.Where(booking => booking.Email.Equals(Text, StringComparison.OrdinalIgnoreCase)).ToList();
+            foreach (var performance in performances)
+            {
+                performance.Concert = concerts.FirstOrDefault(c => c.ID == performance.ConcertID);
+            }
+            foreach (var booking in bookings)
+            {
+                booking.Performance = performances.FirstOrDefault(c => c.ID == booking.PerformanceID);
+            }
+            Bookings = bookings;
         }
-        public BookingsPageVM(IBookingService bookingService)
+        public BookingsPageVM(IBookingService bookingService, IPerformanceService performanceService, IConcertService concertService)
         {
             _bookingService = bookingService;
+            _performanceService = performanceService;
+            _concertService = concertService;
         }
 
     }
